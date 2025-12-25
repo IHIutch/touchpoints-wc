5f609035700830522e5dc5ec0482dded9534abb1
JB Hutch <jbhutch01@gmail.com>
2025-12-24 21:51:28 -0500

    Styled example

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,44 @@
 import { defineConfig } from 'vite'
+import UnoCSS from 'unocss/vite'
+import { presetIcons, presetWind3 } from 'unocss'
+import theme from './theme.json'
 
 export default defineConfig({
   build: {
     lib: {
-      entry: 'form-component.ts',
-      name: 'FormComponent',
-      fileName: 'form-component',
-      formats: ['es']
+      entry: 'src/form-component.ts',
+      formats: ['es'],
+      fileName: 'form-component'
     },
     rollupOptions: {
-      external: [],
-      output: {
-        globals: {}
-      }
+      external: /^lit/
     }
   },
-  server: {
-    open: true
-  }
+  plugins: [
+    UnoCSS({
+      mode: 'shadow-dom',
+      theme: {
+        colors: theme.colors
+      },
+      presets: [
+        // presetWind4({
+        //   preflights: {
+        //     // reset: true,
+        //     // theme: true,
+        //     property: {
+        //       parent: '@layer properties',
+        //       selector: ':where(*, ::before, ::after)'
+        //     }
+        //     // property: {
+        //     //   parent: false
+        //     // }
+        //   }
+        // }),
+        presetWind3(),
+        presetIcons({
+          scale: 1
+        }),
+      ]
+    })
+  ]
 })
